a0894c197040fadd7fc182feaabc15cd3b093d15
Nick Hayes <nhayes559@gmail.com>
2024-03-07 10:57:47 +0000

    deployment test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 
 function App(){
-  
+  React.useEffect(() => {
+    fetch('/api/v1/plants?token=' + import.meta.env.VITE_API_KEY)
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+  })
 
-  return <h1>Hello world</h1>
+  return <section className="section">
+    <div className="container">
+      <h1 className="title">Hello Plants</h1>
+      <img src="https://t4.ftcdn.net/jpg/01/79/88/65/360_F_179886510_6xf0RHhDnLN5ovd2qmGF4WaZMJjqrt6o.jpg" alt="plant" /> 
+    </div>    
+  </section>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {      
+      '/api': {
+        target: 'https://trefle.io/api/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
